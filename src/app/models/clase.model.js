4da7a0004b38684aa2911c1a6527a381c9580386
Edjo31 <eduard31jiron@gmail.com>
2024-03-29 22:44:51 -0600

    add clase.model and clase.router

---
+++ src/app/models/clase.model.js
@@ -0,0 +1,34 @@
+const sequelize = require("../config/sequelizeConect");
+const { DataTypes, Model } = require("sequelize");
+
+class Clase extends Model {}
+
+Clase.init(
+  {
+    idclase: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    nombre_classe: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    descripcion_clase: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    creditos: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "clase",
+    timestamps: false,
+  }
+);
